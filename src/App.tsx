b6b84351ef9bb855529afa7ed492613676eb7568
Robert <oratorio@udel.edu>
2023-03-28 19:30:51 -0400

    fixed bad components - task 9

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): JSX.Element {
                 <b>Made by Robert Oratorio</b>
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
