d6b14aabc98617eadb2cb4d33846c68137982e46
Robert <oratorio@udel.edu>
2023-03-28 16:46:13 -0400

    Added state - task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,93 @@
+/* eslint-disable indent */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "New Years"
+    | "Easter"
+    | "Halloween"
+    | "Thanksgiving"
+    | "Christmas";
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [currentHoliday, setCurrentHoliday] = useState<Holiday>("New Years");
+
+    function getNextHolidayAlphabetically(current: Holiday): Holiday {
+        switch (current) {
+            case "Christmas":
+                // eslint-disable-next-line indent
+                return "Easter";
+            case "Easter":
+                return "Halloween";
+            case "Halloween":
+                return "New Years";
+            case "New Years":
+                return "Thanksgiving";
+            case "Thanksgiving":
+                return "Christmas";
+            default:
+                return current;
+        }
+    }
+
+    function getNextHolidayByTime(current: Holiday): Holiday {
+        switch (current) {
+            case "New Years":
+                return "Easter";
+            case "Easter":
+                return "Halloween";
+            case "Halloween":
+                return "Thanksgiving";
+            case "Thanksgiving":
+                return "Christmas";
+            case "Christmas":
+                return "New Years";
+            default:
+                return current;
+        }
+    }
+
+    // Function to handle click on the Alphabet button
+    function handleAlphabetClick() {
+        const nextHoliday = getNextHolidayAlphabetically(currentHoliday);
+        setCurrentHoliday(nextHoliday);
+    }
+
+    // Function to handle click on the Year button
+    function handleYearClick() {
+        const nextHoliday = getNextHolidayByTime(currentHoliday);
+        setCurrentHoliday(nextHoliday);
+    }
+
+    // View to display the current holiday and buttons
+    return (
+        <div>
+            <div>Holiday: {getHolidayEmoji(currentHoliday)}</div>
+            <Button variant="primary" onClick={handleAlphabetClick}>
+                Advance by Alphabet
+            </Button>
+            <Button variant="secondary" onClick={handleYearClick}>
+                Advance by Year
+            </Button>
+        </div>
+    );
+}
+
+// Helper function to get the emoji for a holiday
+function getHolidayEmoji(holiday: Holiday): string {
+    switch (holiday) {
+        case "New Years": {
+            return "🎊";
+        }
+        case "Easter":
+            return "🐰";
+        case "Halloween":
+            return "🎃";
+        case "Thanksgiving":
+            return "🦃";
+        case "Christmas":
+            return "🎅";
+        default:
+            return "";
+    }
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,37 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+    const [quizInProgress, setQuizInProgress] = useState<boolean>(false);
+
+    const handleStartQuizClick = () => {
+        setNumAttempts(numAttempts - 1);
+        setQuizInProgress(true);
+    };
+
+    const handleStopQuizClick = () => {
+        setQuizInProgress(false);
+    };
+
+    const handleMulliganClick = () => {
+        setNumAttempts(numAttempts + 1);
+    };
+
+    return (
+        <div>
+            <Button
+                onClick={handleStartQuizClick}
+                disabled={quizInProgress || numAttempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={handleStopQuizClick} disabled={!quizInProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={handleMulliganClick} disabled={quizInProgress}>
+                Mulligan
+            </Button>
+            <div>Number of attempts: {numAttempts}</div>
+        </div>
+    );
 }
